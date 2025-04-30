1e5852c6c605e4c3e9450d03df9581e5d4b3b034
Victor Matheus <victormatheus507@gmail.com>
2025-04-30 19:49:55 -0300

    add fonts and colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-2xl">Hello Home</h1>
+      <h1 className="text-2xl text-gray-light">Hello Home</h1>
+      <h2 className="bg-grayDark">ola</h2>
     </div>
   );
 }
