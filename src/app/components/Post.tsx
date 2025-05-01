4f27e59dd13ed00667fbfdd7f864bf175c3d06f0
Victor Matheus <victormatheus507@gmail.com>
2025-05-01 11:37:36 -0300

    feat: post Modal

--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -1,7 +1,12 @@
+'use client';
+
 import Image from 'next/image';
 import Trash from '@/assets/ic_baseline-delete-forever.svg';
 import Edit from '@/assets/bx_bx-edit.svg';
 import { formatDistanceToNowStrict } from 'date-fns';
+import { Modal } from './Modal';
+import { Button, IconButton } from './Button';
+import { useState } from 'react';
 
 export interface PostProps {
   data: {
@@ -14,24 +19,30 @@ export interface PostProps {
 }
 
 export function Post({ data }: PostProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalType, setModalType] = useState<'delete' | 'edit' | null>(null);
+
   return (
     <article className="flex flex-col w-full">
       <header className="flex justify-between items-center p-6 bg-primary rounded-t-2xl border-primary">
         <h2 className="text-[22px] text-white font-bold">{data.title}</h2>
         <div className="flex space-x-6">
-          <Image
-            src={Trash}
-            alt="Delete Post"
-            className="cursor-pointer"
-            height={30}
-            width={30}
+          <IconButton
+            icon={<Image src={Trash} alt="Delete Post" />}
+            aria-label="Delete"
+            onClick={() => {
+              setIsModalOpen(true);
+              setModalType('delete');
+            }}
           />
-          <Image
-            src={Edit}
-            alt="Edit Post"
-            className="cursor-pointer"
-            height={30}
-            width={30}
+
+          <IconButton
+            icon={<Image src={Edit} alt="Edit Post" />}
+            aria-label="Edit"
+            onClick={() => {
+              setIsModalOpen(true);
+              setModalType('edit');
+            }}
           />
         </div>
       </header>
@@ -49,6 +60,44 @@ export function Post({ data }: PostProps) {
 
         <p className="text-lg">{data.content}</p>
       </section>
+
+      {isModalOpen && (
+        <Modal open={isModalOpen} onClose={() => setIsModalOpen(false)}>
+          <h2 className="text-[22px] font-bold">
+            {modalType === 'edit'
+              ? 'Edit Post'
+              : 'Are you sure you want to delete this item?'}
+          </h2>
+          {modalType === 'edit' && (
+            <form>
+              <label className="flex flex-col gap-2">
+                Title
+                <input
+                  className="placeholder:text-sm placeholder:text-gray-light pl-3 py-2 border-1 border-gray-dark rounded-lg"
+                  type="text"
+                  placeholder="Hello world"
+                />
+              </label>
+
+              <label className="flex flex-col gap-2">
+                Content
+                <textarea
+                  className="placeholder:text-sm placeholder:text-gray-light pl-3 py-2 border-1 border-gray-dark rounded-lg resize-none"
+                  placeholder="Content here"
+                />
+              </label>
+            </form>
+          )}
+
+          <section className="flex justify-end gap-4 mt-10">
+            <Button onClick={() => setIsModalOpen(false)}>Cancel</Button>
+
+            <Button styles={modalType === 'edit' ? 'save' : 'danger'}>
+              {modalType === 'edit' ? 'Save' : 'Delete'}
+            </Button>
+          </section>
+        </Modal>
+      )}
     </article>
   );
 }
