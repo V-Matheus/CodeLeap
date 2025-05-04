02948be90d5b0fe3a89d42b73cf3b8b897243ac7
Victor Matheus <victormatheus507@gmail.com>
2025-05-04 18:51:30 -0300

    fix: test Loader

--- __tests__/unit/Loader.test.tsx
+++ __tests__/unit/Loader.test.tsx
@@ -1,15 +1,15 @@
-import { Loader } from "@/components/Loader";
-import { render } from "@testing-library/react";
+import { Loader } from '@/components/Loader';
+import { render } from '@testing-library/react';
 
-describe("Loader", () => {
-  it("should render the Loader component", () => {
+describe('Loader', () => {
+  it('should render the Loader component', () => {
     const screen = render(<Loader />);
 
-    const loader = screen.getByRole("status");
+    const loader = screen.getByRole('status');
     expect(loader).toBeInTheDocument();
     expect(loader).toHaveClass(
-      "inline-block h-6 w-6 animate-spin rounded-full border-4 border-solid border-gray-dark border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]",
+      'inline-block h-6 w-6 animate-spin rounded-full border-4 border-solid border-white border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]',
     );
-    expect(loader).toHaveTextContent("Loading...");
+    expect(loader).toHaveTextContent('Loading...');
   });
-})
+});
